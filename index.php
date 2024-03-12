3b7fcc2e484466c62ecf53100038a44255b4fdd1
AdnanSelimovic <adnanselimovic33@gmail.com>
2024-03-12 06:33:15 +0100

    Milestone 1

--- index.php
+++ index.php
@@ -2,8 +2,43 @@
 require 'vendor/autoload.php'; //run autoloader
 
 Flight::route('/', function(){
-    echo 'Hello world';
+    include 'pages/landing.php';
+});
+
+Flight::route('/registration', function(){
+    include 'pages/registration.php';
+});
+
+Flight::route('/login', function(){
+    include 'pages/login.php';
+});
+
+Flight::route('/home', function(){
+    include 'pages/home.php';
+});
+
+Flight::route('/habits', function(){
+    include 'pages/habits.php';
+});
+
+Flight::route('/habits/edit', function(){
+    include 'pages/edit.php';
+});
+
+Flight::route('/forum', function(){
+    include 'pages/forum.php';
+});
+
+Flight::route('/profile', function(){
+    include 'pages/profile.php';
+});
+
+Flight::route('/info', function(){
+    include 'pages/info.php';
+});
+
+Flight::route('/settings', function(){
+    include 'pages/settings.php';
 });
 
 Flight::start();
-?>
